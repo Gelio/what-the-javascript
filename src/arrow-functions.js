aec8781e2ccbb70c26453cf495aa8b6169c329c3
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-10-07 22:27:13 +0200

    add example on arrow functions

---
+++ src/arrow-functions.js
@@ -0,0 +1,35 @@
+class Foo {
+  constructor() {
+    this.counter = 0;
+  }
+
+  increment() {
+    this.counter++;
+  }
+
+  delayedLog() {
+    // a regular function would have to be bound
+    setTimeout(() => console.log(this.counter), 500);
+  }
+}
+
+const instance = new Foo();
+instance.delayedLog();
+instance.increment();
+
+// mistake with arrow functions and object literals
+
+const literalInstance = {
+  counter: 0,
+  increment: () => {
+    this.counter++;
+    // does not work - this is bound to the outer context (`window`), not `literalInstance`
+  },
+  delayedLog: () => {
+    setTimeout(() => console.log(this.counter), 500);
+  }
+};
+
+literalInstance.increment();
+console.log(literalInstance.counter); // 0
+console.log(window.counter); // NaN
