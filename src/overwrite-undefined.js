8fcf9b150cc1abeca30f9277dcbf87d3546a11dd
Grzegorz Rozdzialik <grzegorz.rozdzialik@codilime.com>
2019-10-08 14:00:04 +0200

    add example on overwriting undefined

---
+++ src/overwrite-undefined.js
@@ -0,0 +1,43 @@
+function example1(undefined) {
+  undefined = true;
+
+  if (undefined) {
+    console.log('Surprised?');
+  }
+}
+
+function example2(undefined) {
+  const obj = {};
+
+  console.log(
+    'obj.nonExistingProperty === undefined',
+    obj.nonExistingProperty === undefined
+  );
+
+  console.log('Overwriting undefined');
+  undefined = true;
+
+  console.log(
+    'obj.nonExistingProperty === undefined',
+    obj.nonExistingProperty === undefined
+  );
+}
+
+function example3(undefined) {
+  console.log('Overwriting undefined');
+  undefined = true;
+  console.log({ undefined });
+
+  console.log('Restoring undefined');
+  undefined = void 0;
+  console.log({ undefined });
+}
+
+console.log('Example 1');
+example1();
+
+console.log('Example 2');
+example2();
+
+console.log('Example 3');
+example3();
