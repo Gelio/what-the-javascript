5df64fd21c275f29604c70abebf16b785286d6ba
Grzegorz Rozdzialik <grzegorz.rozdzialik@codilime.com>
2019-10-08 15:39:17 +0200

    add example on return in try-catch-finally

---
+++ src/return-in-try-catch-finally.js
@@ -0,0 +1,17 @@
+function returnInCatchAndFinally() {
+  try {
+    console.log('Inside try');
+
+    throw new Error();
+  } catch (error) {
+    console.log('Inside catch');
+
+    return 1;
+  } finally {
+    console.log('Inside finally');
+
+    return 2;
+  }
+}
+
+returnInCatchAndFinally();
